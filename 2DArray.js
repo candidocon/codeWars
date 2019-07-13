ac1dae65e7b9120029760f396394e0e36179de4b
candidocon <abrahan64@gmail.com>
2019-07-13 14:07:53 -0400

    2D array

--- 2DArray.js
+++ 2DArray.js
@@ -2,6 +2,7 @@ function namesSorter(departmentsArray) {
   let flatArr = [].concat(...departmentsArray);
   return flatArr.sort((a, b) => {
     if (a.length > b.length) return 1;
+    if (a.length < b.length) return -1;
     if (a.length === b.length) {
       if (a > b) return 1;
       if (a === b) return 0;
