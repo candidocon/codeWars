b59abf4e4dfe1b6c15c2a0b9945a364944fcb826
candidocon <abrahan64@gmail.com>
2019-07-21 13:15:09 -0400

    adding best soution

--- directionReduction.js
+++ directionReduction.js
@@ -49,3 +49,9 @@ function dirReduc(plan) {
 }
 
 //solution 2
+function dirReduc(arr) {
+  var str = arr.join(""),
+    pattern = /NORTHSOUTH|EASTWEST|SOUTHNORTH|WESTEAST/;
+  while (pattern.test(str)) str = str.replace(pattern, "");
+  return str.match(/(NORTH|SOUTH|EAST|WEST)/g) || [];
+}
