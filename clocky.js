8633541ab9855bcef87e2f5887822fe3946b8942
candidocon <abrahan64@gmail.com>
2019-08-07 15:18:27 -0400

    codewars challenge clocky

---
+++ clocky.js
@@ -0,0 +1,7 @@
+var whatTimeIsIt = function(angle) {
+  let hours = Math.floor(angle / 30) === 0 ? 12 : Math.floor(angle / 30);
+  let miuntes = Math.floor((angle % 60) * 2);
+  return `${hours.toString().padStart(2, "0")}:${miuntes
+    .toString()
+    .padStart(2, "0")}`;
+};
