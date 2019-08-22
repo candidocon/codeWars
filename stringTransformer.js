75a25b242db838d53f103fda4e352c120bcb944a
candidocon <abrahan64@gmail.com>
2019-08-22 13:35:55 -0400

    stringTransform first solution

---
+++ stringTransformer.js
@@ -0,0 +1,18 @@
+//https://www.codewars.com/kata/string-transformer/train/javascript
+
+function stringTransformer(str) {
+  let transformedStr = "";
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i].toLowerCase()) {
+      transformedStr += str[i].toUpperCase();
+    } else {
+      transformedStr += str[i].toLowerCase();
+    }
+  }
+  return transformedStr
+    .split(" ")
+    .reverse()
+    .join(" ");
+}
+
+console.log(stringTransformer("Example string")); // 'STRING eXAMPLE';
