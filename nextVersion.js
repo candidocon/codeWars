fe3228ecdbac0ae46de99e1c69a040ec1de688a9
candidocon <abrahan64@gmail.com>
2019-08-27 17:39:17 -0400

    challenge added

--- nextVersion.js
+++ nextVersion.js
@@ -1,1 +1,11 @@
 //https://www.codewars.com/kata/next-version/train/javascript
+
+function nextVersion(version) {
+  let newVersion = version.split(".");
+}
+
+console.log(nextVersion("1.2.3")); // , "1.2.4")
+console.log(nextVersion("0.9.9")); //,"1.0.0")
+console.log(nextVersion("1")); //,"2")
+console.log(nextVersion("1.2.3.4.5.6.7.8")); //,"1.2.3.4.5.6.7.9")
+console.log(nextVersion("9.9")); //,"10.0")
