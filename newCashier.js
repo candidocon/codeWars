58953594808cea97ecb03657e9e6b44bccf2de9c
candidocon <abrahan64@gmail.com>
2019-09-06 17:27:45 -0400

    newCashiers

---
+++ newCashier.js
@@ -0,0 +1,12 @@
+//https://www.codewars.com/kata/new-cashier-does-not-know-about-space-or-shift/train/javascript
+
+function getOrder(input) {
+    return "";
+  }
+
+
+
+console.log(getOrder("milkshakepizzachickenfriescokeburgerpizzasandwichmilkshakepizza"));
+//"Burger Fries Chicken Pizza Pizza Pizza Sandwich Milkshake Milkshake Coke");
+console.log(getOrder("pizzachickenfriesburgercokemilkshakefriessandwich"));
+//"Burger Fries Fries Chicken Pizza Sandwich Milkshake Coke");
