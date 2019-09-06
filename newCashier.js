a7b1086f26b0d2009d9b448ff0bdc9e55ad74862
candidocon <abrahan64@gmail.com>
2019-09-06 17:52:59 -0400

    1st answer

--- newCashier.js
+++ newCashier.js
@@ -1,12 +1,32 @@
 //https://www.codewars.com/kata/new-cashier-does-not-know-about-space-or-shift/train/javascript
 
 function getOrder(input) {
-    return "";
+  let menu = [
+    "Burger",
+    "Fries",
+    "Chicken",
+    "Pizza",
+    "Sandwich",
+    "Onionrings",
+    "Milkshake",
+    "Coke"
+  ];
+  let output = [];
+  let currWord = "";
+  for (let i = 0; i < input.length; i++) {
+    currWord += input[i];
+    let found = menu.findIndex(menuItem => currWord === menuItem.toLowerCase());
+    if (found > -1) {
+      output.push(menu[found]);
+      currWord = "";
+    }
   }
+  return output.join(" ");
+}
 
-
-
-console.log(getOrder("milkshakepizzachickenfriescokeburgerpizzasandwichmilkshakepizza"));
+console.log(
+  getOrder("milkshakepizzachickenfriescokeburgerpizzasandwichmilkshakepizza")
+);
 //"Burger Fries Chicken Pizza Pizza Pizza Sandwich Milkshake Milkshake Coke");
 console.log(getOrder("pizzachickenfriesburgercokemilkshakefriessandwich"));
 //"Burger Fries Fries Chicken Pizza Sandwich Milkshake Coke");
