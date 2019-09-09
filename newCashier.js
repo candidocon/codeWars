dfebcafcf7e2b07c0ca9a82ac3489fc3e558445c
candidocon <abrahan64@gmail.com>
2019-09-08 22:36:50 -0400

    refactored 2

--- newCashier.js
+++ newCashier.js
@@ -22,37 +22,6 @@ function getOrder(input) {
     }
   }
   output.sort((a, b) => {
-    let aPlace = menu.indexOf(a);
-    let bPlace = menu.indexOf(b);
-    if (aPlace < bPlace) return -1;
-    if (aPlace > bPlace) return 1;
-    if (aPlace === bPlace) return 0;
-  });
-  return output.join(" ");
-}
-
-function getOrder(input) {
-  let menu = [
-    "Burger",
-    "Fries",
-    "Chicken",
-    "Pizza",
-    "Sandwich",
-    "Onionrings",
-    "Milkshake",
-    "Coke"
-  ];
-  let output = [];
-  let currWord = "";
-  for (let i = 0; i < input.length; i++) {
-    currWord += input[i];
-    let found = menu.findIndex(menuItem => currWord === menuItem.toLowerCase());
-    if (found > -1) {
-      output.push(menu[found]);
-      currWord = "";
-    }
-  }
-  output.sort((a, b) => {
     return menu.indexOf(a) - menu.indexOf(b);
   });
   return output.join(" ");
