3800e7b0a438ef81cecdc70342e45b152772b740
candidocon <abrahan64@gmail.com>
2019-09-10 15:58:19 -0400

    new kata

--- computeCubeAsSums.js
+++ computeCubeAsSums.js
@@ -1,2 +1,13 @@
 //compute cube as sums
 //https://www.codewars.com/kata/58af1bb7ac7e31b192000020
+
+//given n (where n >= 1) find n consecutive odd numbers whose sum is exactly the cube of n.
+function findSummands(n) {
+  //replace with your solution ...
+  return [1];
+}
+
+console.log(findSummands(3)); // should [7,9,11]
+// because 7 + 9 + 11 = 27, the cube of 3. Note that there are only n = 3 elements in the array.
+
+// write your own tests here ...
