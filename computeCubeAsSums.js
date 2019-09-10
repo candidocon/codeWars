2e55833afff7b9aaf521404955c03b3d5b53861e
candidocon <abrahan64@gmail.com>
2019-09-10 16:05:59 -0400

    solution 1

--- computeCubeAsSums.js
+++ computeCubeAsSums.js
@@ -3,11 +3,14 @@
 
 //given n (where n >= 1) find n consecutive odd numbers whose sum is exactly the cube of n.
 function findSummands(n) {
-  //replace with your solution ...
+  let cube = n * n * n;
+  for (let i = 0; i < cube; i++) {
+    if (i + (i + 2) + (i + 4) === cube) {
+      return [i, i + 2, i + 4];
+    }
+  }
   return [1];
 }
 
 console.log(findSummands(3)); // should [7,9,11]
 // because 7 + 9 + 11 = 27, the cube of 3. Note that there are only n = 3 elements in the array.
-
-// write your own tests here ...
