a5774b87b8770fa4a74771146d00719557ab25aa
candidocon <abrahan64@gmail.com>
2019-09-12 18:03:23 -0400

    commit

--- validParentheses.js
+++ validParentheses.js
@@ -1,2 +1,19 @@
 //Valid Parentheses
 //https://www.codewars.com/kata/valid-parentheses/train/javascript
+
+function validParentheses(parens) {
+  let newParens = [];
+  newParens.push(parens[0]);
+
+  for (let i = 1; i < parens.length; i++) {
+    if (parens[i] === ")") {
+      if (newParens[newParens.length - 1] === "(") {
+        newParens.pop();
+      } else {
+        return false;
+      }
+    }
+    newParens.push(parens[i]);
+  }
+  return newParens.length === 0 ? true : false;
+}
