815d5e77a0a0b217b967606be595b5d4843f075f
candidocon <abrahan64@gmail.com>
2019-09-12 18:11:26 -0400

    best answer

--- validParentheses.js
+++ validParentheses.js
@@ -19,3 +19,24 @@ function validParentheses(parens) {
   }
   return newParens.length === 0 ? true : false;
 }
+
+//best Answer:
+
+// I had something that was smaller and looked cooler, but
+// this is how you'd want to write an actual parser.
+function validParentheses(string) {
+  var tokenizer = /[()]/g, // ignores characters in between; parentheses are
+    count = 0, // pretty useless if they're not grouping *something*
+    token;
+  while (((token = tokenizer.exec(string)), token !== null)) {
+    if (token == "(") {
+      count++;
+    } else if (token == ")") {
+      count--;
+      if (count < 0) {
+        return false;
+      }
+    }
+  }
+  return count == 0;
+}
