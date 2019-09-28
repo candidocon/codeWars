2a763a7f386009c52f271bc31270b5e6e4fd46da
candidocon <abrahan64@gmail.com>
2019-09-28 11:54:13 -0400

    best anwer

--- movingZeroToEnd.js
+++ movingZeroToEnd.js
@@ -9,3 +9,16 @@ var moveZeros = function(arr) {
   }
   return newArr;
 };
+
+//best solution
+var moveZeros = function(arr) {
+  return arr
+    .filter(function(x) {
+      return x !== 0;
+    })
+    .concat(
+      arr.filter(function(x) {
+        return x === 0;
+      })
+    );
+};
