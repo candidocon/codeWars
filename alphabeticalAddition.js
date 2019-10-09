3d1910567f8fc25f642a13777a9106db2999acfd
candidocon <abrahan64@gmail.com>
2019-10-09 12:36:28 -0400

    first solution

--- alphabeticalAddition.js
+++ alphabeticalAddition.js
@@ -2,5 +2,15 @@
 //Alphabetical addition
 
 function addLetters(...letters) {
-  // your code here
+  if (letters.length === 0) {
+    return "z";
+  }
+  let addition = 0;
+  for (let i = 0; i < letters.length; i++) {
+    addition += letters.charCodeAt(i) - 96;
+    if (addition > 26) {
+      addition -= 26;
+    }
+  }
+  return String.fromCharCode(addition + 96);
 }
