1656c5eaf9fa790747bdae6a30787a62f614c438
candidocon <abrahan64@gmail.com>
2019-10-27 15:22:30 -0400

    commit

--- nextVersion.js
+++ nextVersion.js
@@ -14,8 +14,8 @@ function nextVersion(version) {
 }
 
 //test code
-console.log(nextVersion("1.2.3")); // , "1.2.4")
-console.log(nextVersion("0.9.9")); //,"1.0.0")
-console.log(nextVersion("1")); //,"2")
-console.log(nextVersion("1.2.3.4.5.6.7.8")); //,"1.2.3.4.5.6.7.9")
-console.log(nextVersion("9.9")); //,"10.0")
+console.log(nextVersion("1.2.3")); // expected result => "1.2.4"
+console.log(nextVersion("0.9.9")); //=> "1.0.0"
+console.log(nextVersion("1")); //=>"2"
+console.log(nextVersion("1.2.3.4.5.6.7.8")); //=>"1.2.3.4.5.6.7.9"
+console.log(nextVersion("9.9")); // =>"10.0"
